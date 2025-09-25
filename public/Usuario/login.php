011e5f44fd7cf829fb04e307955d48f7b2b9fc94
Pedro <pedropaulo.macedo00@gmail.com>
2025-09-25 00:45:42 -0300

    style: aplica css estilizando paginas form, home e login

--- public/Usuario/login.php
+++ public/Usuario/login.php
@@ -29,6 +29,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <head>
     <meta charset="UTF-8">
     <title>Login</title>
+    <link rel="stylesheet" href="login.css">
 </head>
 <body>
     <h2>Login</h2>
