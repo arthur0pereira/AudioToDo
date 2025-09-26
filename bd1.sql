251508d9d1742cf1fa07ec3717495d018d081d55
arthur0pereira <arthurpereira305@gmail.com>
2025-09-25 21:14:23 -0300

    fix: corregi banco dados

--- bd1.sql
+++ bd1.sql
@@ -62,7 +62,7 @@ CREATE TABLE `usuarios` (
   `id` int(11) NOT NULL,
   `nome` varchar(100) NOT NULL,
   `email` varchar(150) NOT NULL,
-  `senha` varchar(255) NOT NULL,
+  `senha_hash` varchar(255) NOT NULL,
   `data_criacao` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
